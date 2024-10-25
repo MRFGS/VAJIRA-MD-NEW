e108402132f85e78644b3615d0bfd7c62960c557
GOD-SETH <158117163+MRFGS@users.noreply.github.com>
2024-10-25 05:31:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=KcEx1ITY#O1mdfKQS35hXhfpXYgfzBdPl_qDott1DJj0QuXPURjM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=mMtVWTIb#_UIoTe_XrpuYdUSc0tKj0nU9Pcf4KIxztkNa3VSarpc' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94776860011' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
